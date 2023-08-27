e9d62b8321b68bfa5fa2c565c9eb1352e62cc3d3
vaibhav123-dev <vabdarwekar00@gmail.com>
2023-08-27 22:20:27 +0530

    upagte

--- index.js
+++ index.js
@@ -11,26 +11,26 @@ app.use(express.json())
 const PORT = process.env.PORT || 8080
 
 // Schema
-const schemaData = mongoose.Schema({
-    name : String,
-    email : String,
-    mobile : String,
-},{
-    timestamps : true
-})
+// const schemaData = mongoose.Schema({
+//     name : String,
+//     email : String,
+//     mobile : String,
+// },{
+//     timestamps : true
+// })
 
 //model
 
-const userModel = mongoose.model("user",schemaData)
+// const userModel = mongoose.model("user",schemaData)
 
 //read data
 //  http://localhost:8080/
 app.get('/', async (req, res) => {
-    const data = await userModel.find({})
+    // const data = await userModel.find({})
 
     res.json({ success : true ,data:data})
 })
-
+// 
 //create data  || save data
 //  http://localhost:8080/create
 /*
@@ -38,12 +38,12 @@ app.get('/', async (req, res) => {
     email : ,
     mobile : ,
 */
-app.post('/create', async(req,res)=>{
-    console.log(req.body)
-    const data = new userModel(req.body)
-    await data.save( )
-    res.send({success : true , message : "Data save successfully ",data : data})
-})
+// app.post('/create', async(req,res)=>{
+//     console.log(req.body)
+//     const data = new userModel(req.body)
+//     await data.save( )
+//     res.send({success : true , message : "Data save successfully ",data : data})
+// })
 
 //update data
 // http://localhost:8080/update
@@ -54,32 +54,32 @@ app.post('/create', async(req,res)=>{
     email : ,
     mobile : ,
 */
-app.put("/update", async (req ,res)=>{
-    console.log(req.body)
-    const {_id ,...rest} = req.body
+// app.put("/update", async (req ,res)=>{
+//     console.log(req.body)
+//     const {_id ,...rest} = req.body
 
-    console.log(rest)
-    const data = await  userModel.updateOne({_id : _id},rest)
-    res.send({success : true , message : "Data  updated successfully ",data : data})
+//     console.log(rest)
+//     const data = await  userModel.updateOne({_id : _id},rest)
+//     res.send({success : true , message : "Data  updated successfully ",data : data})
 
-})
+// })
 
 //delete
 // http://localhost:8080/delete/id
-app.delete("/delete/:id", async (req ,res)=>{
-   const id = req.params.id
-   console.log(id)
-   const data = await userModel.deleteOne({_id : id})
-   res.send({success : true , message : "Data  deleted successfully ",data : data})
+// app.delete("/delete/:id", async (req ,res)=>{
+//    const id = req.params.id
+//    console.log(id)
+//    const data = await userModel.deleteOne({_id : id})
+//    res.send({success : true , message : "Data  deleted successfully ",data : data})
 
-})
+// })
 
 
 
-mongoose.connect("mongodb://127.0.0.1/crudoperation")
-    .then(() => {
-        console.log("connect to DB")
-        app.listen(PORT, () => console.log("server is running successfully"))
+// mongoose.connect("mongodb://127.0.0.1/crudoperation")
+//     .then(() => {
+//         console.log("connect to DB")
+//         app.listen(PORT, () => console.log("server is running successfully"))
 
-    })
-    .catch((err) => console.log(err))
+//     })
+//     .catch((err) => console.log(err))
